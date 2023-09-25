587c47c819589f603f838dc093b29b89a3ae2a4e
Riko fernalis <140763798+rikofernalis123@users.noreply.github.com>
2023-09-25 09:57:39 -0700

    perubahan di file java script
    perubahan di file javascript

---
+++ js/script.kategori.js
@@ -0,0 +1,31 @@
+var textWrapper = document.querySelector('.judul .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.judul .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.love .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.love',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+
+   
