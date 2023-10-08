f228e5904eadbaded481a5da9d72f0d89bba1616
hauranasy <143099233+hauranasy@users.noreply.github.com>
2023-10-08 09:13:44 +0700

    Create BlogPost.js
    retrieving a list of blog post, retrieving a single blog post by id

---
+++ Backend/models/BlogPost.js
@@ -0,0 +1,19 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  });
+
+  return BlogPost;
+};
